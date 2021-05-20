80aaa1051df90242e3884291d5c3f0ec1eb48e2f
Marcus Xavier <marcusxavierr123@gmail.com>
2021-05-20 20:19:45 -0300

    rename a function

--- channel_posting.py
+++ channel_posting.py
@@ -4,6 +4,7 @@ import json
 import yaml
 from pathlib import Path
 from time import sleep
+import sqlite3
 
 yaml_file = open("config.yaml", 'r')
 yaml_content = yaml.load(yaml_file, Loader = yaml.FullLoader)
@@ -13,6 +14,7 @@ consumer_secret = yaml_content["consumer_secret"]
 access_token = yaml_content["access_token"]
 access_token_secret = yaml_content["access_token_secret"]
 channelId = yaml_content["channelID"]
+databaseName = 'test.sqlite'
 
 def get_twitter_api(tweepy, consumer_key, consumer_secret,access_token,access_token_secret):
     try:
@@ -21,8 +23,6 @@ def get_twitter_api(tweepy, consumer_key, consumer_secret,access_token,access_to
         api = tweepy.API(auth)
         return api
     except Exception as e: 
-        print('entrei')
-        print(e)
         exit(-1)
 
 
@@ -34,14 +34,12 @@ def get_page(page, channelId = channelId, page_size = 50):
     items = json["result"]["items"]
     return items
 
-def get_all(page=1):
+def get_all_pages(page=1):
     all_items = get_page(page)
     if len(all_items) > 0:
         new_page = page + 1
-        return all_items + get_all(new_page)
+        return all_items + get_all_pages(new_page)
     else:
         return all_items
     
-
-
 
